95383db43a41359b4abd244720093f2dad9dd924
benfo <ben.fourie@gmail.com>
2021-06-09 23:39:47 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# CSS Framework Starter
+
+A starter project for building a CSS framework.
+
+## Getting started
+
+1. Clone this repo
+2. Install the dependencies  
+   `yarn` or `yarn install`
+3. Start the development server  
+   `yarn serve`
+4. Make changes to `src\starter.scss` and instantly see the page updates
---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+const { src, dest, watch, series } = require("gulp");
+const del = require("del");
+const sass = require("gulp-sass");
+const browserSync = require("browser-sync").create();
+
+function devBuild() {
+  return src("src/**/*.scss")
+    .pipe(sass({ outputStyle: "expanded" }).on("error", sass.logError))
+    .pipe(dest("site/css"))
+    .pipe(browserSync.stream());
+}
+
+function clean() {
+  return del(["dist"]);
+}
+
+function build() {
+  return src("src/**/*.scss")
+    .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
+    .pipe(dest("dist"));
+}
+
+function serve() {
+  browserSync.init({
+    server: "./site",
+  });
+
+  watch("sass/**/*.scss", devBuild);
+  watch("site/*.html").on("change", browserSync.reload);
+}
+
+exports.build = series(clean, build);
+exports.serve = serve;
